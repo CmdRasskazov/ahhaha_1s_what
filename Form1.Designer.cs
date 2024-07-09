b677a6cb05ba2dc6781e260269c6bf8f49e42f24
lesha <lesha@LAPTOP-0BAI0ISM>
2024-07-09 13:57:54 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace pr_5_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.имяDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.должностьDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.датаРожденияDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.окладDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.сотрудникиBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.companyDataSet = new pr_5_sharp.CompanyDataSet();
+            this.companyDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.сотрудникиTableAdapter = new pr_5_sharp.CompanyDataSetTableAdapters.СотрудникиTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникиBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.имяDataGridViewTextBoxColumn,
+            this.должностьDataGridViewTextBoxColumn,
+            this.датаРожденияDataGridViewTextBoxColumn,
+            this.окладDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.сотрудникиBindingSource;
+            this.dataGridView1.GridColor = System.Drawing.Color.Aqua;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(692, 257);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // имяDataGridViewTextBoxColumn
+            // 
+            this.имяDataGridViewTextBoxColumn.DataPropertyName = "Имя";
+            this.имяDataGridViewTextBoxColumn.HeaderText = "Имя";
+            this.имяDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.имяDataGridViewTextBoxColumn.Name = "имяDataGridViewTextBoxColumn";
+            this.имяDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // должностьDataGridViewTextBoxColumn
+            // 
+            this.должностьDataGridViewTextBoxColumn.DataPropertyName = "Должность";
+            this.должностьDataGridViewTextBoxColumn.HeaderText = "Должность";
+            this.должностьDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.должностьDataGridViewTextBoxColumn.Name = "должностьDataGridViewTextBoxColumn";
+            this.должностьDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // датаРожденияDataGridViewTextBoxColumn
+            // 
+            this.датаРожденияDataGridViewTextBoxColumn.DataPropertyName = "ДатаРождения";
+            this.датаРожденияDataGridViewTextBoxColumn.HeaderText = "ДатаРождения";
+            this.датаРожденияDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.датаРожденияDataGridViewTextBoxColumn.Name = "датаРожденияDataGridViewTextBoxColumn";
+            this.датаРожденияDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // окладDataGridViewTextBoxColumn
+            // 
+            this.окладDataGridViewTextBoxColumn.DataPropertyName = "Оклад";
+            this.окладDataGridViewTextBoxColumn.HeaderText = "Оклад";
+            this.окладDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.окладDataGridViewTextBoxColumn.Name = "окладDataGridViewTextBoxColumn";
+            this.окладDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // сотрудникиBindingSource
+            // 
+            this.сотрудникиBindingSource.DataMember = "Сотрудники";
+            this.сотрудникиBindingSource.DataSource = this.companyDataSet;
+            // 
+            // companyDataSet
+            // 
+            this.companyDataSet.DataSetName = "CompanyDataSet";
+            this.companyDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // companyDataSetBindingSource
+            // 
+            this.companyDataSetBindingSource.DataSource = this.companyDataSet;
+            this.companyDataSetBindingSource.Position = 0;
+            // 
+            // сотрудникиTableAdapter
+            // 
+            this.сотрудникиTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(436, 330);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(692, 254);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Средний оклад по конторе",
+            "Сотрудника по имени",
+            "Сотрудников по должности",
+            "Сотрудников пенсионного возраста",
+            "Сотрудников, чей оклад выше среднего по конторе",
+            "Сотрудников моложе 30-ти лет на указанной должности",
+            "Сортировать список по имени",
+            "Сортировать список по убыванию оклада",
+            "Группировать список по должностям",
+            "Средний оклад по должностям",
+            "Справка об окладах"});
+            this.listBox1.Location = new System.Drawing.Point(773, 43);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(396, 180);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(1183, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выполнить запрос";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 53);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1007, 235);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(25, 126);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(155, 24);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(511, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(46, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "По имени:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(32, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 22);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "По должности:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Location = new System.Drawing.Point(1184, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(208, 180);
+            this.panel1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1216, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Сортировка";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(770, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Средний оклад по конторе:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(890, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 22);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Запросы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(1417, 596);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Как будто Одын эска";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникиBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companyDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource companyDataSetBindingSource;
+        private CompanyDataSet companyDataSet;
+        private System.Windows.Forms.BindingSource сотрудникиBindingSource;
+        private CompanyDataSetTableAdapters.СотрудникиTableAdapter сотрудникиTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn имяDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn должностьDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn датаРожденияDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn окладDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace pr_5_sharp
+{
+    public partial class Form1 : Form
+    {
+        DataTable myTable;
+        public Form1()
+        {
+            InitializeComponent();
+            myTable = companyDataSet.Tables[0];
+        }
+
+
+        void LookAt(IEnumerable<DataRow> value)
+        {
+            //dataGridView2.DataSource = null;
+            //dataGridView2.Columns.Add("Column1", "Имя");
+            //dataGridView2.Columns.Add("Column2", "Должность");
+            //dataGridView2.Columns.Add("Column3", "ДатаРождения");
+            //dataGridView2.Columns.Add("Column4", "Оклад");
+
+            //IEnumerable<DataRow> myQuery = myTable.AsEnumerable();
+            //foreach (DataRow tmp in value)
+            //{
+            //    dataGridView2.Rows.Add(tmp.Field<string>("Имя"),
+            //    tmp.Field<string>("Должность"),
+            //    tmp.Field<DateTime>("ДатаРождения"),
+            //    tmp.Field<int>("Оклад"));
+            //}
+
+
+            var ForLook = value.AsEnumerable().Select(t => new
+            {
+                Name = t.Field<string>("Имя"),
+                Job = t.Field<string>("Должность"),
+                DateBirth = t.Field<DateTime>("ДатаРождения"),
+                Salary = t.Field<int>("Оклад"),
+
+            }).ToList();
+            dataGridView2.DataSource = ForLook;
+        }
+        public static int ReturnAge(DateTime dateBirth)
+        {
+            int age = (int)(DateTime.Now - dateBirth).TotalDays / 365;
+            return age;
+            
+        }
+        //• средний оклад по конторе;
+        //• сотрудника по имени;
+        //• сотрудников по должности;
+        //• сотрудников пенсионного возраста;
+        //• сотрудников, чей оклад выше среднего по конторе;
+        //• сотрудников моложе 30-ти лет на указанной должности.
+
+        //Используйте LINQ to DataSet, чтобы:
+        //• сортировать список по фамилии;
+        //• сортировать список по убыванию оклада;
+        //• группировать список по должностям.
+        //• найти средний оклад по должностям.
+        //сформировать справку об окладах в виде: фамилия – оклад с сортировкой по убыванию
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "companyDataSet.Сотрудники". При необходимости она может быть перемещена или удалена.
+            this.сотрудникиTableAdapter.Fill(this.companyDataSet.Сотрудники);
+
+            var MySelectName = myTable.AsEnumerable().Select(t => t.Field<string>("Имя")).Distinct();
+            comboBox1.DataSource = MySelectName.ToList();
+
+            var MySelectJob = myTable.AsEnumerable().Select(t => t.Field<string>("Должность")).Distinct();
+            comboBox2.DataSource = MySelectJob.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Columns.Clear();
+            if (listBox1.SelectedItem != null)
+            {
+                switch(listBox1.SelectedItem.ToString())
+                {
+                    case "Средний оклад по конторе":
+                        
+                        var average_salary = myTable.AsEnumerable().Average(t => t.Field<int>("Оклад"));
+                        textBox2.Text = Math.Round( average_salary, 0).ToString();
+                        break;
+
+                    case "Сотрудника по имени":
+                        label1.Text = "Сотрудники по имени: " + comboBox1.Text;
+                        IEnumerable<DataRow> query1 =
+                            from t in myTable.AsEnumerable()
+                            where (t.Field<string>("Имя").Contains(comboBox1.Text))
+                            select t;
+                        LookAt(query1);
+                        break;
+
+                    case "Сотрудников по должности":
+                        label1.Text = "Сотрудники по должности: " + comboBox2.Text;
+                        IEnumerable<DataRow> query2 =
+                            from t in myTable.AsEnumerable()
+                            where (t.Field<string>("Должность").Contains(comboBox2.Text))
+                            select t;
+                        LookAt(query2);
+                        break;
+
+                    case "Сотрудников, чей оклад выше среднего по конторе":
+                        label1.Text = "Сотрудники, чей оклад выше среднего по конторе: ";
+                        double sum = myTable.AsEnumerable().Average(order => order.Field<int>("Оклад"));
+                        IEnumerable<DataRow> query3 = (from t in myTable.AsEnumerable()
+                                                       where t.Field<int>("Оклад") > sum
+                                                       select t);
+                        LookAt(query3);
+                        break;
+
+                    case "Сотрудников пенсионного возраста":
+                        label1.Text = "Сотрудники пенсионного возраста: ";
+                        IEnumerable<DataRow> query0 = (from t in myTable.AsEnumerable()
+                                                       where ReturnAge( t.Field<DateTime>("ДатаРождения")) > 65
+                                                       select t);
+
+                        LookAt(query0);
+                        break;
+
+                    case "Сотрудников моложе 30-ти лет на указанной должности":
+                        label1.Text = "Сотрудники моложе 30-ти лет на должности:  " + comboBox2.Text;
+                        IEnumerable<DataRow> query4 = (from t in myTable.AsEnumerable()
+                                                       where t.Field<string>("Должность").Contains(comboBox2.Text) && ReturnAge(t.Field<DateTime>("ДатаРождения")) < 30
+                                                       select t);
+                        LookAt(query4);
+                        break;
+
+                    case "Сортировать список по имени":
+                        label1.Text = "Сортировка по имени ";
+                        IEnumerable<DataRow> query5 = myTable.AsEnumerable().OrderBy(t => t.Field<string>("Имя"));
+                        LookAt(query5);
+                        break;
+
+                    case "Сортировать список по убыванию оклада":
+                        label1.Text = "Сортирока по убыванию оклада ";
+                        IEnumerable<DataRow> query6 = myTable.AsEnumerable().OrderByDescending(t => t.Field<int>("Оклад"));
+                        LookAt(query6);
+                        break;
+
+                    case "Группировать список по должностям":
+                        label1.Text = "Группировка по должностям ";
+                        var query7 = myTable.AsEnumerable().GroupBy(t => t.Field<string>("Должность")).Select(g => new
+                        {
+                            Job = g.Key,
+                            Total = g.Count(),
+                            Employees = string.Join(", ", g.Select(p => p.Field<string>("Имя")))
+                        }).ToList();
+                        dataGridView2.DataSource = query7;
+                        break;
+
+                    case "Средний оклад по должностям":
+                        label1.Text = "Средний оклад по должностям ";
+                        var query8 = myTable.AsEnumerable().GroupBy(t => t.Field<string>("Должность")).Select(g => new
+                        {
+                            Job = g.Key,
+                            Total = g.Count(),
+                            Avg_Salary = Math.Round(g.Average(t => t.Field<int>("Оклад")))
+                        }).ToList();
+                        dataGridView2.DataSource = query8;
+                        break;
+
+                    case "Справка об окладах":
+                        label1.Text = "Справка о доходах ";
+                        var query9 = myTable.AsEnumerable().Select(t => new
+                        {
+                            Name = t.Field<string>("Имя"),
+                            Salary = t.Field<int>("Оклад"),
+                        }).OrderByDescending(p => p.Salary).ToList();
+                        dataGridView2.DataSource = query9;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+
+            switch (listBox1.SelectedItem.ToString())
+            {
+                case "Сотрудника по имени":
+                    comboBox1.Enabled = true;
+                    break;
+
+                case "Сотрудников моложе 30-ти лет на указанной должности":
+                case "Сотрудников по должности":
+                    comboBox2.Enabled = true;
+                    break;
+            }
+        }
+    }
+}
